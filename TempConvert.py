05014e758060d49d5628326940d11a79834fd563
liamgillispie <l.gillispie526@gmail.com>
2026-02-07 21:38:17 +0000

    Converted F to C!

--- TempConvert.py
+++ TempConvert.py
@@ -1,17 +1,23 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name:Liam Gillispie
+#Date:2/8/26
+#Assignment:Lab 3
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
+  print("Hello User!")
+  name=input("What is your name User?")
+  print(f"Welcome {name}")
+  temp_f=input("What is the temperature outside in Fahenheit?")
   #Convert that temperature to celsius, rounding to 1 decimal percision
+
   #Output converted temperature.
-  tempF = 80
+  tempF=float(temp_f)
 
-  tempC = tempF / 2
+  tempC=round((tempF-32)*5/9,0)
 
-  print(tempF, "is ", tempC, "degrees celsius.")
+  print(f"{tempF}F is {tempC}C")
 if __name__ == '__main__':
-  main()
+    main()
+    
